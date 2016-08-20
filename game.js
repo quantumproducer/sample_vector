50eda0891c379e5b525232df6c0a7e8b06e7405c
quantumpotato <quantumpotato@gmail.com>
2016-08-20 12:30:08 -0400

    Inertia setup

--- game.js
+++ game.js
@@ -4,8 +4,8 @@ class ComboRockets extends Game {
 	setupPlayers() {
 		this.players = [];
 		var p = new Ship();
-		p.x = 50;
-		p.y = 50;
+		p.x = 450;
+		p.y = 450;
 		this.players.push(p);
 		this.add('ships', p);
 	}
--- pilots.js
+++ pilots.js
@@ -6,10 +6,10 @@ class Ship extends Thing {
 	}
 
 	init(o) {
-		this.r = 0;
+		this.r = 65;
 		var controls = new ShipControls({'ship' : this});
 		this.install('controls', controls);
-		var inertia = new Inertia({'mass' : 10, 'theta' : 90, 'thing' : this});
+		var inertia = new Inertia({'mass' : 10, 'theta' : 130, 'magnitude' : 30, 'thing' : this});
 		this.install('inertia', inertia);
 		this.install('thruster', new Thruster({'ship' : this}));
 		this.install('turner', new Turner({'ship' : this}));
@@ -48,18 +48,85 @@ class ShipControls extends Component {
 			this.ship.grab('turner').turnRight();
 		}
 
-		applyThrust(this.ship);	
+		this.applyThrust();
+	}
+
+	applyThrust() {
+		var t1 = this.ship.r; //rotation, the theta the ship is facing
+		var m1 = this.ship.grab('thruster').power;
+
+		var inertia = this.ship.grab('inertia');
+		var t2 = inertia.theta;
+		var m2 = inertia.magnitude;
+		
+		// console.log(m2);
+
+		//Kim is Inertia
+		//Noah is Thrust
+		var kimVX = (m2 * Math.cos(t2 * Math.PI / 180));
+		var kimVY = (m2 * Math.sin(t2 * Math.PI / 180));
+
+		// console.log(kimVX);
+		// console.log(kimVY);
+		// return;
+
+		var noahVX = (m1 * Math.cos(t1 * Math.PI / 180));
+		var noahVY = (m1 * Math.sin(t1 * Math.PI / 180));
+
+		// console.log(noahVX);
+		// console.log(noahVY);
+		// return;
+
+		var resultVX = kimVX + noahVX;
+		var resultVY = kimVY + noahVY;
+
+		// console.log(resultVX);
+		// console.log(resultVY);
+		// return;
+
+		var resultMagnitude = Math.sqrt(resultVX * resultVX + resultVY * resultVY);
+		// console.log(resultMagnitude);
+
+		var resultTheta = Math.atan( Math.abs(resultVY) / Math.abs(resultVX) );
+		console.log(resultTheta);
+
+		inertia.magnitude = resultMagnitude;
+		inertia.theta = resultTheta;
+		return;
+
+		// console.log(kimVX);
+		// console.log(noahVX);
+		// return;
+
+		console.log(resultVectorX);
+		// console.log(resultVectorY);
+
+		var c = (resultVectorX * resultVectorX) + (resultVectorY + resultVectorY);
+		var resultMagnitude = Math.sqrt(c);
+		
+		// console.log(resultMagnitude);
+
+		var atan = Math.atan(resultVectorY / resultVectorX);
+		// console.log(atan);
+		// var resultTheta = atan * 180 / Math.PI;
+
+		// console.log(resultTheta);
+
+		inertia.magnitude = resultMagnitude;
+		inertia.theta = atan;
 	}
 }
 
 class Thruster extends Component {
 	constructor(o) {
 		super(o);
-		this.power = 0;
+		this.power = 40;
 		this.maxPower = 5;
 	}
 
 	accelerate() {
+		this.power = 40;
+		return;
 		this.power++;
 		if (this.power > this.maxPower) {
 			this.power = this.maxPower;
